48cb3fc06223928683ceb01d11ab33cb845ca97b
Ravin <ravinhwg@gmail.com>
2021-09-12 14:59:17 +0530

    Noif background handler

--- index.js
+++ index.js
@@ -5,8 +5,15 @@
 import {AppRegistry, LogBox} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging';
 
 // Ignore react-query timeout warnings
 LogBox.ignoreLogs(['Setting a timer']);
 
+// Register background handler
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
+
+AppRegistry.registerComponent('app', () => App);
 AppRegistry.registerComponent(appName, () => App);
